62c08e1fc8341afa1ef12766045ef6bfd8b98089
vijay@vjh1.com <root@ip-172-31-17-247.us-east-2.compute.internal>
2020-01-30 19:03:36 +0000

    fff

--- generate.sh
+++ generate.sh
@@ -5,7 +5,6 @@ while i=1
 	dnsvalidator -tL https://raw.githubusercontent.com/BBerastegui/fresh-dns-servers/master/resolvers.txt -threads 20 -o resolvers.txt
 	git add resolvers.txt
 	git commit -m "resolvers.txt" "resolvers.txt";
-	git pull origin master;
 	git push origin master;
 	sleep 30m;
  done
